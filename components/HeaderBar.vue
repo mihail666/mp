c8873f5a027819ed002e8a15683343884a0c0eb8
Mihail FR <76650506+mihail666@users.noreply.github.com>
2023-12-03 21:49:09 +0500

    sidebar

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,55 +1,63 @@
 <template>
   <v-card>
-    <v-navigation-drawer
-
-      class="nav-bg"
-    >
-      <v-list nav>
-        <v-list-item
-          prepend-icon="mdi-email"
-          title="Inbox"
-          value="inbox"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account-supervisor-circle"
-          title="Supervisors"
-          value="supervisors"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-clock-start"
-          title="Clock-in"
-          value="clockin"
-        ></v-list-item>
+    <v-navigation-drawer width="315" height="900" class="nav-bg">
+      <v-list nav class="border ">
+        <v-row class="">
+          <v-col :cols="page.cols" v-for="page of pages" class="border">
+            <v-list-item
+            
+              :prepend-icon="page.icon"
+              :title="page.title"
+              @click="navigateTo(`${page.url}`)"
+              stacked
+              class=""
+              width=""
+            >
+          </v-list-item>
+          </v-col>
+        </v-row>
       </v-list>
     </v-navigation-drawer>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { useTheme } from 'vuetify'
+import { useTheme } from "vuetify";
 
-const theme = useTheme()
+const theme = useTheme();
 
 const languages = ref([
-  { icon: 'material-symbols:language-us', title: 'English' },
-  { icon: 'material-symbols:language-spanish-rounded', title: 'Spanish' },
-  { icon: 'material-symbols:language-french', title: 'French' },
-])
-const selectedLanguage = ref(languages.value[0])
+  { icon: "material-symbols:language-us", title: "English" },
+  { icon: "material-symbols:language-spanish-rounded", title: "Spanish" },
+  { icon: "material-symbols:language-french", title: "French" },
+]);
+const selectedLanguage = ref(languages.value[0]);
 
 const toggleTheme = () => {
   theme.global.name.value =
-    theme.global.name.value == MAIN_THEME ? MAIN_DARK_THEME : MAIN_DARK_THEME
-    // theme.global.name.value == MAIN_THEME ? MAIN_DARK_THEME : MAIN_THEME
-}
+    theme.global.name.value == MAIN_THEME ? MAIN_DARK_THEME : MAIN_DARK_THEME;
+  // theme.global.name.value == MAIN_THEME ? MAIN_DARK_THEME : MAIN_THEME
+};
 
-const darkTheme = computed(() => theme.current.value.dark)
+const darkTheme = computed(() => theme.current.value.dark);
+
+const pages = ref([
+  { icon: "fluent:person-24-regular", title: "Profile", url: "/profile", cols: "6" },
+  { icon: "solar:bag-5-linear", title: "Cart", url: "/shoppingCart", cols: "6" },
+  { icon: "fluent:settings-24-regular", title: "Settings", url: "/settings", cols: "6" },
+  { icon: "fluent:person-24-regular", title: "Profile", url: "/profile", cols: "6" },
+  { icon: "solar:bag-5-linear", title: "Cart", url: "/shoppingCart", cols: "6" },
+  { icon: "fluent:settings-24-regular", title: "Settings", url: "/settings", cols: "6" },
+  { icon: "fluent:person-24-regular", title: "Profile", url: "/profile", cols: "6" },
+  { icon: "solar:bag-5-linear", title: "Cart", url: "/shoppingCart", cols: "6" },
+  { icon: "fluent:settings-24-regular", title: "Settings", url: "/settings", cols: "12" }
+]);
 
 const profileActions = ref([
-  { icon: 'fluent:person-24-regular', title: 'Profile', url: '/profile' },
-  { icon: 'solar:bag-5-linear', title: 'Cart', url: '/shoppingCart' },
-  { icon: 'fluent:settings-24-regular', title: 'Settings', url: '/settings' },
-])
+  { icon: "fluent:person-24-regular", title: "Profile", url: "/profile" },
+  { icon: "solar:bag-5-linear", title: "Cart", url: "/shoppingCart" },
+  { icon: "fluent:settings-24-regular", title: "Settings", url: "/settings" },
+]);
 </script>
 
 <style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <template>
   <MainLayout class="bg-background overflow-hidden">
     <template v-if="true">
-      <div class="h-100 position-relative">
+      <div class="h-screen position-relative bg-primary">
+        <img src="../assets/images/stars.png" class="stars stars-left" alt="" />
+        <img
+          src="../assets/images/stars.png"
+          class="stars stars-right"
+          alt=""
+        />
+        <img src="../assets/images/shape.png" class="shape-img" alt="" />
+      </div>
+      <div class="h-screen position-relative">
+        <img src="../assets/images/bg-2.png" class="bg-bright" alt="" />
+      </div>
+      <!-- <div class="h-100 position-relative">
         <div class="">
-          <div class="position-relative">
-            <v-row>
+          <div class="position-relative h-screen">
+            <v-row class="">
               <v-col>
                 <img
                   src="../assets/images/stars.png"
@@ -39,11 +51,11 @@
             />
           </div>
         </div>
-      </div>
+      </div> -->
     </template>
   </MainLayout>
 </template>
 
 <script setup lang="ts">
-import MainLayout from '~/layouts/MainLayout.vue'
+import MainLayout from "~/layouts/MainLayout.vue";
 </script>
